df733856c9adf569b58ed1cc007f4f528e9fbb02
Valdeva Crema <ives1227@yahoo.com>
2017-03-08 12:30:39 -0500

    More precise error message for lack of authorization.  Redirects to job.

--- frontend/routes.rb
+++ frontend/routes.rb
@@ -1,8 +1,6 @@
 ArchivesSpace::Application.routes.draw do
   match '/plugins/linkomatic/fetch_digital_objects' => 'linkomatic#fetch_digital_objects', :via => [:get]
-  match '/plugins/linkomatic/in_fetch_session' => 'linkomatic#in_fetch_session', :via => [:get]
-  match '/plugins/linkomatic/clear_fetch_session' => 'linkomatic#clear_fetch_session', :via => [:get]
-  match '/plugins/linkomatic/digital_objects_fetched' => 'linkomatic#digital_objects_fetched', :via => [:get]
+  match '/plugins/linkomatic/view_job' => 'linkomatic#view_job', :via => [:get]
   match('/plugins/fetch_urns' => 'fetch_urns#index', :via => [:get])
   match('/plugins/fetch_urns/create' => 'fetch_urns#create', :via => [:post])
       
